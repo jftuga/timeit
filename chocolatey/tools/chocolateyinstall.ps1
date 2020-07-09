002a7fbf12883fbbd2de1c468a167398cf04c937
John Taylor <johntaylor@gmail.com>
2020-07-09 07:59:45 -0400

    update chocolatey to v1.2.3

--- chocolatey/tools/chocolateyinstall.ps1
+++ chocolatey/tools/chocolateyinstall.ps1
@@ -1,7 +1,7 @@
 ﻿
 $ErrorActionPreference = 'Stop';
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url64      = 'https://github.com/jftuga/timeit/releases/download/v1.2.1/timeit_1.2.1_windows_amd64.zip'
+$url64      = 'https://github.com/jftuga/timeit/releases/download/v1.2.3/timeit_1.2.3_windows_amd64.zip'
 
 $packageArgs = @{
   packageName   = $env:ChocolateyPackageName
@@ -11,7 +11,7 @@ $packageArgs = @{
   url64bit      = $url64
 
   softwareName  = 'timeit*'
-  checksum64    = 'bc8d55cdd5cdd8a83e0411792b74891758f390986eeaf442e7e50720d8f38009'
+  checksum64    = 'e1b0b0b608d0a87a8cae58e6bd4de6d43be47d9377cdab45148693db3d015206'
   checksumType64= 'sha256'
 
   validExitCodes= @(0)
